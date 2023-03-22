062627bfa2065aaab54b8f4d03f1ee4dbb7f6f73
shl2587 <smsg2587@gmail.com>
2023-03-22 23:30:40 +0900

    static class phonebook

--- Day09/src/phonebook/Contact.java
+++ Day09/src/phonebook/Contact.java
@@ -1,5 +1,9 @@
 package phonebook;
 
 public class Contact {
-
+	
+	int idx;
+	String pnum;
+	String name;
+	
 }
---
+++ Day09/src/phonebook/Main.java
@@ -0,0 +1,9 @@
+package phonebook;
+
+public class Main {
+	static Contact[] arr = new Contact[5];
+	
+	public static void main(String[] args) {
+		
+	}
+}
